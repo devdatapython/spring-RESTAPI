39c323ab35d3500fcba408330ca2a863c6757740
Pedro <pedrofreitasengenharia@gmail.com>
2020-11-08 09:30:02 -0300

    Add role attribute

--- src/main/java/com/springcourse/domain/RequestStage.java
+++ src/main/java/com/springcourse/domain/RequestStage.java
@@ -1,5 +1,22 @@
 package com.springcourse.domain;
 
+import java.util.Date;
+
+import com.springcourse.domain.enums.RequestState;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestStage {
+	private Long id;
+	private String description;
+	private Date realizationDate;
+	private RequestState state;
+	private Request request;
+	private User user;
 
 }
